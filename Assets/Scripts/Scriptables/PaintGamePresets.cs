c37537c3944846205d23b37fccd14529eb82ec5a
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2021-11-16 02:13:38 +0000

    Add game presets  scriptable object

---
+++ Assets/Scripts/Scriptables/PaintGamePresets.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New PaintGamePreset", menuName = "PaintMeUP/Paint Game Preset", order = 0)]
+public class PaintGamePresets : ScriptableObject 
+{
+
+    [SerializeField] private int _secondsPerColor;
+    public int SecondsPerColor {get => _secondsPerColor;}
+
+    [SerializeField] private int _extraEndSeconds;
+    public int ExtraEndSeconds {get => _extraEndSeconds;}
+
+    public int GameDuration {get => _secondsPerColor + _extraEndSeconds;}
+
+    [SerializeField] private GameColor[] _colorsToPaint;
+
+
+
+}

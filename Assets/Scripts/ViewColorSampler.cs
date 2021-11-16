f7f8e84fc5dfedf391d4b8ebef11166cbdf37164
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2021-11-16 20:30:31 +0000

    Sample color from onscreen pixels

---
+++ Assets/Scripts/ViewColorSampler.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ViewColorSampler : MonoBehaviour 
+{
+    [SerializeField] private RenderTexture _viewRenderTexture;
+    public Color CaptureColorOnScreen(float x, float y)
+    {
+        x = Mathf.Clamp01(x);
+        y = Mathf.Clamp01(y);
+         // Get the image on screen and put it to a Texture2D to compare colors
+        RenderTexture.active = _viewRenderTexture;
+        
+        Texture2D texture = new Texture2D(_viewRenderTexture.width,_viewRenderTexture.height);
+        
+        texture.ReadPixels(new Rect(0,0,texture.width,texture.height),0,0,false);
+        texture.Apply(false);
+        //textureTestPlane.SetTexture("RenderToText",texture);
+        //textureTestPlane.mainTexture =  texture;
+        
+        // Matches the current color's range with the center of the view image
+        Color textureColor = texture.GetPixel(
+        (int)(texture.width * x),
+        (int)(texture.height * y ));
+        RenderTexture.active = null;
+        //Debug.Log(textureColor);
+        return textureColor;
+        
+     
+        
+       
+    }
+
+}

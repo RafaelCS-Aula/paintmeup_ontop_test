923ff631f52740efef9c3e8490105ece60d85dde
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2021-11-29 13:46:05 +0000

    Comment redundant code

--- Assets/Scripts/TrackedImageObjectSpawner.cs
+++ Assets/Scripts/TrackedImageObjectSpawner.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.Events;
 
 public class TrackedImageObjectSpawner : MonoBehaviour
 {
 
     [SerializeField] ARTrackedImageManager _trackedImageManager;
-    [SerializeField] GameObject _prefabToSpawn;
+    
+    // Uncomment to use manual spawning 
+   /* [SerializeField] GameObject _prefabToSpawn;
 
     private GameObject _spawnedPrefab;
+    */
+
+    [SerializeField] private UnityEvent OnImageFound;
 
     // Start is called before the first frame update
     void Awake()
@@ -22,27 +28,31 @@ public class TrackedImageObjectSpawner : MonoBehaviour
             Debug.LogError("Need a ARTrackedImageManager component");
             this.enabled = false;
         }
-            
+    
     }
 
     private void OnEnable() {
+        
         _trackedImageManager.trackedImagesChanged += ImageTracked;
     }
     private void OnDisable() {
         _trackedImageManager.trackedImagesChanged -= ImageTracked;
+        
     }
 
     private void ImageTracked(ARTrackedImagesChangedEventArgs eventArgs)
     {
-
-        foreach(ARTrackedImage image in eventArgs.added)
+        // Uncomment to use manual spawning
+      /*  foreach(ARTrackedImage image in eventArgs.added)
         {
             _spawnedPrefab = Instantiate(_prefabToSpawn,image.transform.position, Quaternion.identity);
             _spawnedPrefab.transform.LookAt(Camera.main.transform,transform.up);
+            OnImageFound.Invoke();
+
         }
         foreach(ARTrackedImage image in eventArgs.updated)
         {
             _spawnedPrefab.transform.position = image.transform.position;
-        }
+        }*/
     }
 }
